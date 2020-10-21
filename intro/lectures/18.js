e0e7156e8a7d73d55325b73728650e6a6d15eb02
Daniel Haehn <haehn@mpsych.org>
2020-10-21 10:18:24 -0400

    Ready for lecture 18.

---
+++ intro/lectures/18.js
@@ -0,0 +1,26 @@
+frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
+times = [0, // Today... 
+         1600, // 
+         4500, // And
+         6500, // 
+         9200, // this is lecture 19
+         15000];
+
+
+document.getElementById("content").innerHTML = "\
+    <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
+\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will learn about colors...</div>\
+\
+    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
+\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will start with our virtual robot!</div>\
+\
+    <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 18.</div> \
+<audio id='voice'>    \
+    <source src='lectures/"+lecturenumber+".mp3'>    \
+  </audio>    \
+</div>   \
+\
+";
+
