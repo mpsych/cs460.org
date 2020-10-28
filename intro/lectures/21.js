aef5e3eb63a668ecdf29bb553aa9329c92617d0c
Daniel Haehn <haehn@mpsych.org>
2020-10-28 09:11:48 -0400

    Ready for lecture 21.

---
+++ intro/lectures/21.js
@@ -0,0 +1,27 @@
+frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
+times = [0, // Today... 
+         1000, // 
+         3500, // And
+         4500, // 
+         7300, // this is lecture 19
+         13000];
+
+
+document.getElementById("content").innerHTML = "\
+    <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
+\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will look at animations..</div>\
+\
+    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
+\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will learn more about interpolation!</div>\
+\
+    <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 21.</div>    \
+\
+<audio id='voice'>    \
+    <source src='lectures/"+lecturenumber+".mp3'>    \
+  </audio>    \
+</div>   \
+\
+";
+
