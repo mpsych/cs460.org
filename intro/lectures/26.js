c7d1f7e60685e2a677c7e0d1d7feb318d66b7d8f
Daniel Haehn <haehn@mpsych.org>
2022-11-07 10:37:20 -0500

    Update Jasmine's intro.

--- intro/lectures/26.js
+++ intro/lectures/26.js
@@ -16,7 +16,7 @@ document.getElementById("content").innerHTML = "\
 \
     <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We have Jasmine Roberts as our guest.</div>\
 \
-    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>Jasmine is an AR/VR specialist and currently getting her PhD at the University of Washington.</div>\
+    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>Jasmine is an AR/VR specialist and currently at the University of Washington and Microsoft.</div>\
 \
     <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>Before, she worked at Google Creative Labs in NYC, Unity, NASA, Playstation, and Intel.</div>\
 \
@@ -24,9 +24,9 @@ document.getElementById("content").innerHTML = "\
 \
     <div id='content6' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>Jasmine also was a Fellow at Oculus Launch Pad and Mozilla XR Studios.</div>\
 \
-    <div id='content7' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>She will speak to us about her career in computer graphics.</div>\
+    <div id='content7' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>She will speak to us about generative worlds!</div>\
 \
-    <div id='content8' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 29.</div>    \
+    <div id='content8' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 26.</div>    \
 <audio id='voice'>    \
     <source src='lectures/none.mp3'>    \
   </audio>    \
