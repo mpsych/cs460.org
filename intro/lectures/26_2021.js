b765dc200a6c268bb2d39c9258858946bd92de38
Daniel Haehn <haehn@mpsych.org>
2022-11-07 10:36:19 -0500

    Update.

---
+++ intro/lectures/26_2021.js
@@ -0,0 +1,26 @@
+frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
+times = [0, // Today... 
+         1000, // 
+         4000, // And
+         5500, // 
+         8500, // this is lecture 19
+         12000];
+
+
+document.getElementById("content").innerHTML = "\
+    <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
+\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will continue exploring geometries..</div>\
+\
+    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
+\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will load meshes from files!</div>\
+\
+    <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 26.</div>  \
+<audio id='voice'>    \
+    <source src='lectures/"+lecturenumber+".mp3'>    \
+  </audio>    \
+</div>   \
+\
+";
+
