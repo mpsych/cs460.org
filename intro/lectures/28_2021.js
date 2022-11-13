5e20b679cab6b5c4fe630b4c53ffcf0b378acbd6
Daniel Haehn <haehn@mpsych.org>
2022-11-13 18:16:21 -0500

    Update.

--- intro/lectures/28_2021.js
+++ intro/lectures/28_2021.js
@@ -1,20 +1,19 @@
 frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
 times = [0, // Today... 
-         1500, // 
-         5500, // And
-         7500, // 
-         11500, // this is lecture 19
-         17000];
-
+         900, // 
+         3000, // And
+         3700, // 
+         7400, // this is lecture 19
+         12000];
 
 document.getElementById("content").innerHTML = "\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will continue exploring geometries..</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will learn about lighting..</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will look at different materials!</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will combine lighting and different materials!</div>\
 \
     <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 28.</div>    \
 <audio id='voice'>    \
