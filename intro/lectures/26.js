b4118b254284f14ed575523c41edcff2bc07060a
Daniel Haehn <haehn@mpsych.org>
2023-11-06 08:45:59 -0500

    Upd.

--- intro/lectures/26.js
+++ intro/lectures/26.js
@@ -14,17 +14,17 @@ document.getElementById("content").innerHTML = "\
 \
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We have Jasmine Roberts as our guest.</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We have Manuel Sainsily as our guest.</div>\
 \
-    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>Jasmine is an AR/VR specialist and currently at the University of Washington and Microsoft.</div>\
+    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>Manuel is an UX/HCI/XR specialist based in Montreal, currently working for Unity.</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>Before, she worked at Google Creative Labs in NYC, Unity, NASA, Playstation, and Intel.</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>Before, he worked at Immersion, IBM, Fans Entertainment, and Mindgeek.</div>\
 \
-    <div id='content5' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>Jasmine studied at Columbia, the MIT Media Lab, and the Harvard School of Design.</div>\
+    <div id='content5' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>Manu studied Computer Science at SUPINFO International University.</div>\
 \
-    <div id='content6' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>Jasmine also was a Fellow at Oculus Launch Pad and Mozilla XR Studios.</div>\
+    <div id='content6' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>He is a Carribean-artist based in Montreal.</div>\
 \
-    <div id='content7' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>She will speak to us about generative worlds!</div>\
+    <div id='content7' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>And he will speak to us about his career and projects!</div>\
 \
     <div id='content8' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 26.</div>    \
 <audio id='voice'>    \
