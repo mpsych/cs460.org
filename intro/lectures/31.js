ed406e9a4fa4e4380709bc79e89b2b54cf0ae3e8
Daniel Haehn <haehn@mpsych.org>
2023-11-17 09:39:37 -0500

    Upd.

--- intro/lectures/31.js
+++ intro/lectures/31.js
@@ -9,13 +9,13 @@ times = [0, // Today...
 document.getElementById("content").innerHTML = "\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will talk about final projectssssss..</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will talk about final projects..</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
     <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>learn about volume rendering!</div>\
 \
-    <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 30.</div>    \
+    <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 31.</div>    \
 \
 <audio id='voice'>    \
     <source src='lectures/"+lecturenumber+".mp3'>    \
