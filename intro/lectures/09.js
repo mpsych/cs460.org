7c4e15d57411739c97b29a9f28368b46a05cde86
Daniel Haehn <haehn@mpsych.org>
2024-10-01 08:49:28 -0400

    Update lecture 9.

--- intro/lectures/09.js
+++ intro/lectures/09.js
@@ -9,11 +9,11 @@ times = [0, // Today...
 document.getElementById("content").innerHTML = "\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will leave all frameworks behind...</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will code the indexed geometry...</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>start with default WebGL!</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>add moving rectangles!</div>\
 \
     <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 9.</div>\    \
   <audio id='voice'>    \
