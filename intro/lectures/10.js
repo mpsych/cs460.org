5c497c4c4b3a709659517f81b12cac17fc163737
Daniel Haehn <haehn@mpsych.org>
2024-10-02 23:01:59 -0400

    Update lecture 10 intro.

--- intro/lectures/10.js
+++ intro/lectures/10.js
@@ -10,11 +10,11 @@ document.getElementById("content").innerHTML = "\
 \
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will continue our WebGL code...</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will add multiple rectangles...</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will learn about rendering primitives, indexed geometries, and other tricks!</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>make them bounce!</div>\
 \
     <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 10.</div>    \
   <audio id='voice'>    \
