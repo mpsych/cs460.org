6a0a006a7b93a5b6fa44226000356360c007ec70
Daniel Haehn <haehn@mpsych.org>
2024-10-10 12:14:12 -0400

    Intro for lecture 12.

--- intro/lectures/12.js
+++ intro/lectures/12.js
@@ -1,23 +1,25 @@
 frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
 times = [0, // Today... 
-         2000, // 
-         5500, // And
-         7000, // 
-         10000, // this is lecture 10
-         15000];
+         1500, // 
+         5000, // And
+         6800, // 
+         11000, // this is lecture 10
+         16000];
 
 document.getElementById("content").innerHTML = "\
+\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will code more WebGL...</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will review what we learned in the last week...</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>Catch up with shaders and animation!</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will start with a transformation matrix!</div>\
 \
     <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 12.</div>    \
+\
 <audio id='voice'>    \
-    <source src='lectures/12.mp3'>    \
+    <source src='lectures/13.mp3'>    \
   </audio>    \
 </div>   \
 \
