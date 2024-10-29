e7d6ad949643b3616afc3ac59e1c53192dd66bdf
Daniel Haehn <haehn@mpsych.org>
2024-10-29 10:14:12 -0400

    Update.

--- intro/lectures/allyssa.js
+++ intro/lectures/allyssa.js
@@ -11,15 +11,15 @@ times = [0, // Today...
 document.getElementById("content").innerHTML = "\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>the wonderful Allyssa Lewis is our guest!</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>the wonderful Allyssa A Lewis is our guest!</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>She will tell us</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>about life as an animator</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>how AI impacts her life as an animator</div>\
 \
-    <div id='content5' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>and help you find jobs!</div>\
+    <div id='content5' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>and how to build a strong profile!</div>\
 \
-    <div id='content6' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is the first CS Seminar in Fall 2022!</div>    \
+    <div id='content6' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 17!</div>    \
 \
 <audio id='voice'>    \
     <source src='lectures/"+lecturenumber+".mp3'>    \
