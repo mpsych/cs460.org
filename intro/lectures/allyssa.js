b8c562534cbfac0ada5ec2fedc203fed7d53db3b
Daniel Haehn <haehn@mpsych.org>
2024-10-29 10:36:16 -0400

    Update.

--- intro/lectures/allyssa.js
+++ intro/lectures/allyssa.js
@@ -1,25 +1,21 @@
-frames = ['content1', 'content2', 'content3', 'content4', 'content5', 'content6', null];
+frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
 times = [0, // Today... 
-         1500, // 
-         4500, // And
-         8000, // 
-         10300, // this is lecture 19
-         13000, //
-         20000];
-
+         1600, // 
+         6000, // And
+         7500, // 
+         11800, // this is lecture 10
+         17000];
 
 document.getElementById("content").innerHTML = "\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
     <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>the wonderful Allyssa A Lewis is our guest!</div>\
 \
-    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>She will tell us</div>\
+    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>how AI impacts her life as an animator</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will learn how AI impacts her life as an animator!</div>\
 \
-    <div id='content5' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>and how to build a strong profile!</div>\
-\
-    <div id='content6' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 17!</div>    \
+    <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 17.</div>   \
 \
 <audio id='voice'>    \
     <source src='lectures/"+lecturenumber+".mp3'>    \
