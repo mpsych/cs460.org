d693cada579cee983922d1635c8d2a5d77c6902b
Daniel Haehn <haehn@mpsych.org>
2024-11-05 10:19:05 -0500

    Update.

--- intro/lectures/19.js
+++ intro/lectures/19.js
@@ -9,11 +9,11 @@ times = [0, // Today...
 document.getElementById("content").innerHTML = "\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will repeat what we learned so far..</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will learn about normals..</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will continue with the virtual robot!</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>Mesh file formats!</div>\
 \
     <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 19.</div>    \
 <audio id='voice'>    \
