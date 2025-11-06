f74e7c1b74e0cd44532748b1dcb25ebc64a67fd0
Daniel Haehn <haehn@mpsych.org>
2025-11-05 22:31:33 -0500

    Upd.

--- intro/lectures/18.js
+++ intro/lectures/18.js
@@ -1,22 +1,21 @@
 frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
 times = [0, // Today... 
-         1600, // 
-         4500, // And
-         6500, // 
-         9200, // this is lecture 19
+         1000, // 
+         4300, // And
+         5300, // 
+         9000, // this is lecture 19
          15000];
-
 
 document.getElementById("content").innerHTML = "\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will look at the inkman again...</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will learn about materials!</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>learn about the anaglyph effect!</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>Change the appearance of the armadillo!</div>\
 \
-    <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 18.</div> \
+    <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 18.</div>    \
 <audio id='voice'>    \
     <source src='lectures/"+lecturenumber+".mp3'>    \
   </audio>    \
