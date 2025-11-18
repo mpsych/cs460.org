2cfa464466b05010834637bd463a19201942529d
Daniel Haehn <haehn@mpsych.org>
2025-11-18 08:00:13 -0500

    Upd

--- intro/lectures/20.js
+++ intro/lectures/20.js
@@ -1,19 +1,21 @@
 frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
 times = [0, // Today... 
          1000, // 
-         4300, // And
-         5300, // 
-         9000, // this is lecture 19
-         15000];
+         4000, // And
+         5500, // 
+         8500, // this is lecture 19
+         12000];
+
 
 document.getElementById("content").innerHTML = "\
+\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will do some VR..</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We talk about the final project..</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will learn about materials!</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>look at medical visualizations!</div>\
 \
     <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 20.</div>    \
 <audio id='voice'>    \
