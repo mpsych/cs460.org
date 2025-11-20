1f8b46836d880bfbb99c7cf902b5b4c4515882ae
Daniel Haehn <haehn@mpsych.org>
2025-11-19 22:54:15 -0500

    Upd.

--- intro/lectures/21.js
+++ intro/lectures/21.js
@@ -1,20 +1,20 @@
 frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
 times = [0, // Today... 
-         1000, // 
-         5500, // And
-         6500, // 
-         9000, // this is lecture 19
-         13000];
+         1500, // 
+         4000, // And
+         5500, // 
+         8500, // this is lecture 19
+         12000];
 
 
 document.getElementById("content").innerHTML = "\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will learn about lighting..</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will talk about NeRFs..</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We will talk about textures!</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>the glTF file format!</div>\
 \
     <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 21.</div>    \
 \
