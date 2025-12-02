a6fd5a84769cab797b4e13e5ac3b8406a908fce6
Daniel Haehn <haehn@mpsych.org>
2025-12-02 08:55:27 -0500

    Upd.

--- intro/lectures/23.js
+++ intro/lectures/23.js
@@ -1,6 +1,6 @@
 frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
 times = [0, // Today... 
-         1500, // 
+         1000, // 
          4000, // And
          5500, // 
          8500, // this is lecture 19
@@ -8,16 +8,16 @@ times = [0, // Today...
 
 
 document.getElementById("content").innerHTML = "\
+\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will talk about NeRFs..</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will acquire some 360 footage..</div>\
 \
     <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
 \
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>the glTF file format!</div>\
+    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>We render it!</div>\
 \
     <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 23.</div>    \
-\
 <audio id='voice'>    \
     <source src='lectures/"+lecturenumber+".mp3'>    \
   </audio>    \
