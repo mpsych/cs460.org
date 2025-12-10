735a79a5752071d462cfb95645acd36169bf4860
Daniel Haehn <haehn@mpsych.org>
2025-12-10 17:09:45 -0500

    Upd.

--- intro/lectures/26.js
+++ intro/lectures/26.js
@@ -1,23 +1,16 @@
 frames = ['content1', 'content2', 'content3', 'content4', 'content5', null];
 times = [0, // Today... 
-         1000, // 
-         4000, // And
-         5500, // 
-         8500, // this is lecture 19
-         12000];
+         1100, // 
+         4400, // And
+         5400];
 
-
+     
 document.getElementById("content").innerHTML = "\
-\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will acquire some more 360 footage..</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will have our final round of presentations!</div>\
 \
-    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>And...</div>\
-\
-    <div id='content4' style='display:none;position: absolute;color:white;top:35%;left:20%;margin-left:50px;font-size:440%'>render some videos as well!</div>\
-\
-    <div id='content5' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 26.</div>    \
+    <div id='content3' style='display:none;position: absolute;color:white;top:45%;left:40%;margin-left:50px;font-size:440%'>This is lecture 26.</div>    \
 <audio id='voice'>    \
     <source src='lectures/"+lecturenumber+".mp3'>    \
   </audio>    \
