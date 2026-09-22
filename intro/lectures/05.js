0e0d35dd73a67da794041ae18785258599f0115f
Daniel Haehn <haehn@mpsych.org>
2026-09-22 08:29:11 -0400

    Upd.

--- intro/lectures/05.js
+++ intro/lectures/05.js
@@ -10,9 +10,9 @@ times = [0, // Today...
 document.getElementById("content").innerHTML = "\
     <div id='content1' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>Today...</div>\
 \
-    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will talk about</div>\
+    <div id='content2' style='display:none;position: absolute;color:white;top:30%;left:20%;margin-left:50px;font-size:440%'>We will keep</div>\
 \
-    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>the current assignment.</div>\
+    <div id='content3' style='display:none;position: absolute;color:white;top:20%;left:10%;margin-left:50px;font-size:440%'>working with cubes.</div>\
 \
     <div id='content4' style='display:none;position: absolute;color:white;top:25%;left:30%;margin-left:50px;font-size:440%'>And..</div>\
 \
